bd09d12f1443242da75a5a4f36715d8b58410ae2
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-04-01 09:16:37 +0200

    Some basic mech parts.

--- lib/gls_agent/gls_mech.rb
+++ lib/gls_agent/gls_mech.rb
@@ -1,5 +1,17 @@
 require 'mechanize'
+require 'logger'
 
 class GLSMech
+  attr_accessor :mech
 
+  # Setup the mech.
+  def initialize
+    @mech = Mechanize.new
+    @mech.user_agent_alias = 'Windows Mozilla'
+  end
+
+  # Enable logging to given file.
+  def log_to filename
+    @mech.log = Logger.new filename
+  end
 end

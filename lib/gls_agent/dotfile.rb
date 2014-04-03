42422c9664499f3931c081e965884b07f40c68c2
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-04-03 18:07:16 +0200

    Get rid of intermediate json conversion for dotfile (what a weird ideagit add lib/gls_agent/dotfile.rb)

--- lib/gls_agent/dotfile.rb
+++ lib/gls_agent/dotfile.rb
@@ -1,26 +1,32 @@
 module GLSAgent
+
+  def self.hash_from_text text
+    hash = {}
+    text.each_line do |line|
+      fields = line.split('=')
+      fields.each &:strip!
+      fields.each &:rstrip!
+      hash[fields[0].to_sym] = fields[1]
+    end
+    hash
+  end
+
+  # Get keys and values from file in ~/.gls_agent and put them in a hash.
+  # keys are symbols.
+  # The file should follow the syntax
+  #     option=value
+  #     option2=value2
+  # Meaningful keys are i.e. user and pass.
   def self.get_dotfile_opts
     options = {}
     # Get defaults from ~/.gls_agent
     begin
       filecontent = File.open("#{Dir.home}/.gls_agent").read
-      # from option=value\noption2=value2 make {'option':'value','option2':'value2'}
-      filecontent.gsub!(/ *= */, ':')
-      filecontent.gsub!(/(\w+)/, '"\1"')
-      filecontent.gsub!("\n", ",\n")
-      filecontent.chomp!("\n")
-      filecontent.chomp!(',')
-      filecontent = "{ #{filecontent} }"
-      begin
-        j_options = JSON.parse filecontent
-        j_options.each {|o,v| options[o.to_sym] = v}
-        puts "Info: read configuration parameters from ~/.gls_agent, may be overriden with cmd line options."
-      rescue
-        STDERR.puts "Error: configuration file in ~/.gls_agent found but could not be parsed."
-        STDERR.puts $!.inspect,$@
-      end
+      options = hash_from_text filecontent
+      puts "Info: read configuration parameters from ~/.gls_agent, may be overriden with cmd line options."
     rescue
       STDERR.puts "Info: No configuration file in ~/.gls_agent found, all options need to be specified on command line."
+      #STDERR.puts $!.inspect,$@
     end
     options
   end  

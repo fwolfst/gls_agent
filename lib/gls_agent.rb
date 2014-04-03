ec5292286acf241ef2c1dde658a803cfe12cbec4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-04-03 18:06:30 +0200

    Require dotfile when requiring gls_agent, added ParcelJob creation helpers.

--- lib/gls_agent.rb
+++ lib/gls_agent.rb
@@ -1,6 +1,20 @@
 require "gls_agent/version"
 require 'gls_agent/gls_mech'
+require 'gls_agent/dotfile'
 
 module GLSAgent
   ParcelJob = Struct.new(:name, :street, :streetno, :zip, :city, :weight)
+
+  def self.job_from_csv string
+    fields = string.split(',')
+    if fields.length != 6
+      fail 'job_from_csv needs 6 fields'
+      return nil
+    end
+    ParcelJob.new(*fields)
+  end
+
+  def self.job_from_hash hash
+    ParcelJob.new(hash[:name], hash[:street], hash[:streetno], hash[:zip], hash[:city], hash[:weight])
+  end
 end

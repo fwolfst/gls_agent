c1e906b0a52f397b8ea148331eafcc7f118f314e
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-04-22 09:37:49 +0200

    spec for 8th parcel field (company name).

--- spec/gls_agent_spec.rb
+++ spec/gls_agent_spec.rb
@@ -2,21 +2,22 @@ require_relative '../lib/gls_agent'
 
 describe 'GLSAgent Module helpers' do
   it 'creates ParcelJob from csv string' do
-    job = GLSAgent::job_from_csv '02.02.2014,Name,Street,1,1234,City,1'
+    job = GLSAgent::job_from_csv '02.02.2014,Name,Company,Street,1,1234,City,1'
     expect(job.date).to eq '02.02.2014'
     expect(job.name).to eq 'Name'
+    expect(job.company).to eq 'Company'
     expect(job.street).to eq 'Street'
     expect(job.streetno).to eq '1'
     expect(job.zip).to eq '1234'
     expect(job.city).to eq 'City'
     expect(job.weight).to eq '1'
   end
-
   it 'creates ParcelJob from hash' do
-    hash = {:date => '02.12.2012', :name => 'Name', :street => 'Street', :streetno => '1', :zip => '1234', :city => 'City', :weight => '1'}
+    hash = {:date => '02.12.2012', :name => 'Name', :company => 'Company', :street => 'Street', :streetno => '1', :zip => '1234', :city => 'City', :weight => '1'}
     job = GLSAgent::job_from_hash hash
     expect(job.date).to eq '02.12.2012'
     expect(job.name).to eq 'Name'
+    expect(job.company).to eq 'Company'
     expect(job.street).to eq 'Street'
     expect(job.streetno).to eq '1'
     expect(job.zip).to eq '1234'
